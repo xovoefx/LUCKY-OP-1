8c840f432b6070caa2910983f311592acbbb3448
Lucky boy ❤️ <94621036+Mrvk1703@users.noreply.github.com>
2022-02-26 11:05:41 +0530

    Update Downloader.py

--- Hero/Core/PyTgCalls/Downloader.py
+++ Hero/Core/PyTgCalls/Downloader.py
@@ -38,7 +38,7 @@ def download(videoid: str, mystic, title) -> str:
                 try:
                     if eta > 2:
                         mystic.edit(
-                            f"**{percentage} ▓▓▓▓▓▓▓▓▓▓▓▓ 100%"
+                            f"**{percentage} ■■■ 100%"
                         )
                 except Exception as e:
                     pass
@@ -47,21 +47,21 @@ def download(videoid: str, mystic, title) -> str:
                     flex[str(bytesx)] += 1
                     if eta > 2:
                         mystic.edit(
-                            f"**{percentage} ███▓▓▓▓▓▓▓▓▓ 100%"
+                            f"**{percentage} ■■■■■ 100%"
                         )
             if per > 500:
                 if flex[str(bytesx)] == 3:
                     flex[str(bytesx)] += 1
                     if eta > 2:
                         mystic.edit(
-                            f"**{percentage} ██████▓▓▓▓▓▓ 100%"
+                            f"**{percentage} ■■■■■■■ 100%"
                         )
             if per > 800:
                 if flex[str(bytesx)] == 4:
                     flex[str(bytesx)] += 1
                     if eta > 2:
                         mystic.edit(
-                            f"**{percentage} ██████████▓▓ 100%"
+                            f"**{percentage} ■■■■■■■■■ 100%"
                         )
         if d["status"] == "finished":
             try:
@@ -70,7 +70,7 @@ def download(videoid: str, mystic, title) -> str:
                 taken = "00:00"
             size = d["_total_bytes_str"]
             mystic.edit(
-                f"**100% ████████████100%"
+                f"**100% ■■■■■■■■■■100%"
             )
 
     ydl_optssx = {

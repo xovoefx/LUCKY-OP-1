84d64601b6c41ecf2f8895b803727c34e69515ce
Lucky boy ❤️ <94621036+Mrvk1703@users.noreply.github.com>
2022-03-06 07:23:49 +0530

    Update Stats.py

--- Hero/Plugins/Stats.py
+++ Hero/Plugins/Stats.py
@@ -60,7 +60,7 @@ async def gstats(_, message):
         pass
     uptime = await bot_sys_stats()
     response = await message.reply_photo(
-        photo="Utils/Query.jpg", caption="Getting Stats!"
+        photo=f"https://telegra.ph/file/3f09576ad17907503d2fc.jpg", caption="Getting Stats!"
     )
     end = datetime.now()
     resp = (end - start).microseconds / 1000

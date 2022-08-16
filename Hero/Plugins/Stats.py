6c6ff890429fdef87a945494e2ac4d5074808420
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-08-16 18:34:10 +0530

    Update Stats.py

--- Hero/Plugins/Stats.py
+++ Hero/Plugins/Stats.py
@@ -60,7 +60,7 @@ async def gstats(_, message):
         pass
     uptime = await bot_sys_stats()
     response = await message.reply_photo(
-        photo="Utils/Query.jpg", caption="Getting Stats!"
+        photo=f"https://telegra.ph/file/efe1ffdb64e5c8b444578.jpg", caption="Getting Stats!"
     )
     end = datetime.now()
     resp = (end - start).microseconds / 1000
